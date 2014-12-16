14b078d6dda547df517a0041aaf8aa25bda2f0a2
MrDarkFatali <mrdarkfatali@gmail.com>
2014-12-16 14:07:37 +0100

    Ejercicio 8 PHP

---
+++ Ejercicio8/Ejercicio8/pregunta3b.php
@@ -0,0 +1,16 @@
+<!doctype html>
+	<head>
+		<title>Pregunta 3</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h3>3ª. ¿Cuál de las siguientes opciones es una famosa página para compartir fotografías?</h3>
+		<form name="formulario" action="estadisticasb.php?id=pregunta3" method="post">
+			<input type="radio" name="respuesta" value="a" />Youtube<br/>
+			<input type="radio" name="respuesta" value="b" />Instagram<br/>
+			<input type="radio" name="respuesta" value="c" />Vimeo<br/>
+			<input type="radio" name="respuesta" value="d" />Themeforest<br/>
+			<input type="submit" name="siguiente" value="Siguiente" />
+		</form>
+	</body>
+</html>
